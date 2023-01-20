85cac0b908a051032b0eb49aa46c4b94d24f0cb9
gabrielrsh <gabriel.shiota@gmail.com>
2023-01-20 14:42:57 -0300

    adiciona exemplos em operadores unarios

--- java-basico/src/Operadores/OperadoresUnarios.java
+++ java-basico/src/Operadores/OperadoresUnarios.java
@@ -3,6 +3,23 @@ package Operadores;
 public class OperadoresUnarios {
     
     public static void main(String[] args) {
+        // Operador unário
+        int numero = 5;
+        boolean afirmacao = true;
+        
+        // negação "!" para valores booleanos e "-" para inverter sinal valores numericos
+        afirmacao = !afirmacao;
+        numero = - numero;
+
+        System.out.println(numero);
+        System.out.println(afirmacao);
+        
+        numero = - numero;
+        afirmacao = !afirmacao;
+        System.out.println(numero);
+        System.out.println(afirmacao);
+        
+    //-----------------------------------    
         int numero1, numero2;
         numero1 = numero2 = 5;
         // Operador pos fixado
@@ -15,5 +32,6 @@ public class OperadoresUnarios {
         //Operador pre fixado
         System.out.println(++numero1);
         System.out.println(--numero2);
+        
     }
 }
